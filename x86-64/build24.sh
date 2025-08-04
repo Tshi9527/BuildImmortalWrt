13274e4ff7409abe6b0a46bead7a0c92cff1db94
Tshi9527 <165747803+Tshi9527@users.noreply.github.com>
2025-08-04 23:03:08 +0800

    Update build24.sh

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -60,7 +60,6 @@ PACKAGES="$PACKAGES luci-app-openclash"
 #PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-zerotier"
 PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
 	
 
